059ddb6aeba05ce5de04161d1b8b6e334472118c
Sanele <Sanele@LAPTOP-FC04BNU2>
2024-01-20 08:38:07 +0200

    Add project files.

---
+++ 3.Simple arithmetic/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ScrollBar
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("3+7 =" & 3 + 7)
+        MessageBox.Show("1-5 =" & 1 - 5)
+        MessageBox.Show("11*2 =" & 11 * 2)
+        MessageBox.Show("17/4 =" & 17 / 4)
+
+
+    End Sub
+End Class
